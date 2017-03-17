413ea6aea9d1c03e190a26f7932e7f4d6065c9f3
Mircea Ulinic <mirucha@cloudflare.com>
2017-03-17 14:52:45 +0000

    Add log transport

---
+++ napalm_logs/transport/log.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+'''
+Log transport for napalm-logs.
+Send logging events across the network.
+'''
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+# Import stdlib
+import json
+import logging, logging.handlers
+
+# Import napalm-logs pkgs
+from napalm_logs.transport.base import TransportBase
+
+
+class LogTransport(TransportBase):
+    '''
+    Log transport class.
+    '''
+    def __init__(self, addr, port):
+        self.addr = addr
+        self.port = port
+
+    def start(self):
+        self.logger = logging.getLogger('napalm-logs')
+        self.logger.setLevel(logging.INFO)
+        handler = logging.handlers.SocketHandler(self.addr, self.port)
+        formatter = logging.Formatter('%(asctime)s: %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    def serialise(self, obj):
+        return json.dumps(obj)
+
+    def publish(self, obj):
+        self.logger.info(
+            self.serialise(obj)
+        )
