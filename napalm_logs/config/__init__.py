8f4b6a9ec0cbbcc43124c57955a3b5f800c95123
Mircea Ulinic <mirucha@cloudflare.com>
2017-03-30 11:40:13 +0000

    Move VALID_CONFIG into config/__init__

--- napalm_logs/config/__init__.py
+++ napalm_logs/config/__init__.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
 '''
-Device worker process
+Config defaults.
 '''
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-DEFAULT_DELIM = '|'
+DEFAULT_DELIM = '/'
+
+VALID_CONFIG = {
+    'prefix': {
+        'values': {
+            'error': basestring
+        },
+        'line': basestring
+    },
+    'messages': {
+        '*': {
+            'values': dict,
+            'line': basestring,
+            'model': basestring,
+            'mapping': dict
+        }
+    }
+}
