65d95ccb66895d6a798a44e69d6f150006a2a5a9
Mircea Ulinic <mirceaulinic@users.noreply.github.com>
2017-06-14 11:36:07 +0100

    Version 0.0.1b7

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='napalm-logs',
-    version='0.0.1b5',
+    version='0.0.1b7',
     packages=find_packages(),
     author='Mircea Ulinic',
     author_email='mircea.ulinic@gmail.com',
