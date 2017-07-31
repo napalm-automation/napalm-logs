c882cde3fedee4d428cd1515b803f0e0dcfaf794
Mircea Ulinic <mircea.ulinic@gmail.com>
2017-07-31 15:09:20 +0000

    Prepare tests for base, dev, pub and srv

---
+++ tests/test_base.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+'''
+Test the napalm-logs base class.
+'''
+from __future__ import absolute_import
+from __future__ import unicode_literals
---
+++ tests/test_server.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+'''
+Test the server process.
+'''
+from __future__ import absolute_import
+from __future__ import unicode_literals
