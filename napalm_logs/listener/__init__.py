9595744b4c40e734116bce99d12dd27b3a5c7c3c
Mircea Ulinic <mircea.ulinic@gmail.com>
2017-07-31 11:04:19 +0000

    Raise exception when transport/listener unavailable

--- napalm_logs/listener/__init__.py
+++ napalm_logs/listener/__init__.py
@@ -5,11 +5,19 @@ napalm-logs pluggable listener.
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+# Import python std lib
+import logging
+
 # Import napalm-logs pkgs
+# Exceptions
+from napalm_logs.exceptions import InvalidListenerException
+# Listener classes
 from napalm_logs.listener.kafka import KafkaListener
 from napalm_logs.listener.tcp import TCPListener
 from napalm_logs.listener.udp import UDPListener
 from napalm_logs.listener.kafka import HAS_KAFKA
+
+log = logging.getLogger(__file__)
 
 LISTENER_LOOKUP = {
     'tcp': TCPListener,
@@ -18,6 +26,7 @@ LISTENER_LOOKUP = {
 }
 
 if HAS_KAFKA:
+    log.info('Kafka dependency seems to be installed, making kafka listener available.')
     LISTENER_LOOKUP['kafka'] = KafkaListener
 
 
@@ -25,7 +34,13 @@ def get_listener(name):
     '''
     Return the listener class.
     '''
-    return LISTENER_LOOKUP.get(name, LISTENER_LOOKUP['*'])
+    try:
+        log.debug('Using %s as listener', name)
+        return LISTENER_LOOKUP[name]
+    except KeyError:
+        msg = 'Listener {} is not available.'.format(name)
+        log.error(msg, exc_info=True)
+        raise InvalidListenerException(msg)
 
 
 __all__ = (
--- napalm_logs/transport/__init__.py
+++ napalm_logs/transport/__init__.py
@@ -5,7 +5,13 @@ napalm-logs pluggable publisher.
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+# Import python std lib
+import logging
+
 # Import napalm-logs pkgs
+# Exceptions
+from napalm_logs.exceptions import InvalidTransportException
+# Transport classes
 from napalm_logs.transport.zeromq import ZMQTransport
 from napalm_logs.transport.cli import CLITransport
 from napalm_logs.transport.log import LogTransport
@@ -14,6 +20,8 @@ from napalm_logs.transport.kafka import HAS_KAFKA
 from napalm_logs.transport.kafka import KafkaTransport
 # from napalm_logs.transport.kafka import KafkaTransport
 # from napalm_logs.transport.rabbitmq import RabbitMQTransport
+
+log = logging.getLogger(__file__)
 
 TRANSPORT_LOOKUP = {
     'zeromq': ZMQTransport,
@@ -24,10 +32,11 @@ TRANSPORT_LOOKUP = {
     'log': LogTransport,
     # 'rmq': RabbitMQransport,
     # 'rabbitmq': RabbitMQransport,
-    '*': ZMQTransport  # default transport
+    '*': ZMQTransport
 }
 
 if HAS_KAFKA:
+    log.info('Kafka dependency seems to be installed, making kafka transport available.')
     TRANSPORT_LOOKUP['kafka'] = KafkaTransport
 
 
@@ -35,7 +44,13 @@ def get_transport(name):
     '''
     Return the transport class.
     '''
-    return TRANSPORT_LOOKUP.get(name, TRANSPORT_LOOKUP['*'])
+    try:
+        log.debug('Using %s as transport', name)
+        return TRANSPORT_LOOKUP[name]
+    except KeyError:
+        msg = 'Transport {} is not available.'.format(name)
+        log.error(msg, exc_info=True)
+        raise InvalidTransportException(msg)
 
 
 __all__ = (
