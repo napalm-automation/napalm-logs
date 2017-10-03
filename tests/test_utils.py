3d119e4b6fdf1199ebf4bd80e066a286c271bae0
John Anderson <lampwins@gmail.com>
2017-10-03 10:42:29 -0400

    added tests for bgp_state_convert_function

---
+++ tests/test_utils.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+'''
+Test the napalm-logs base class.
+'''
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from napalm_logs.utils import bgp_state_convert
+
+
+def test_bgp_state_convert_in_dict():
+    """
+    Test bgp_state_convert returns values from its internal dict
+    """
+    assert bgp_state_convert('OpenSent') == 'OPEN_SENT'
+    assert bgp_state_convert('OpenConfirm') == 'OPEN_CONFIRM'
+
+
+def test_bgp_state_convert_not_dict():
+    """
+    Test bgp_state_convert returns upper values for items not in
+    its internal dict
+    """
+    assert bgp_state_convert('Connect') == 'CONNECT'
