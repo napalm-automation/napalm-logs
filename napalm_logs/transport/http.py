4b20a755120db8f82f29fb868fc3a47ade4916fc
Mircea Ulinic <mirucha@cloudflare.com>
2017-10-29 19:50:38 +0000

    Start a limited number of threads for requests

--- napalm_logs/transport/http.py
+++ napalm_logs/transport/http.py
@@ -8,6 +8,10 @@ from __future__ import unicode_literals
 # Import stdlib
 import logging
 import threading
+try:
+    import Queue as queue
+except ImportError:
+    import queue
 
 # Import third party libs
 try:
@@ -18,6 +22,7 @@ except ImportError:
 
 try:
     import tornado
+    import tornado.httpclient
     HAS_TORNADO = True
 except ImportError:
     HAS_TORNADO = False
@@ -44,66 +49,118 @@ class HTTPTransport(TransportBase):
         else:
             self.address = address
         self.method = kwargs.get('method', 'GET')
-        log.debug('Publishing to %s using %s', self.address, self.method)
+        log.debug('Publishing to %s using method %s', self.address, self.method)
         self.auth = kwargs.get('auth')
-        username = kwargs.get('username')
-        password = kwargs.get('password')
+        self.username = kwargs.get('username')
+        self.password = kwargs.get('password')
         if not self.auth:
-            if username and password:
-                self.auth = (username, password)
+            if self.username and self.password:
+                self.auth = (self.username, self.password)
         self.headers = kwargs.get('headers', {})
         self.verify_ssl = kwargs.get('verify_ssl', True)
         self.params = kwargs.get('params')
+        self.max_clients = kwargs.get('max_clients', 10)
         self.backend = kwargs.get('backend')
         if not self.backend:
-            log.debug('No explicit backend requested')
+            log.info('No explicit backend requested')
             if HAS_TORNADO:
                 self.backend = 'tornado'
-                log.debug('Executing asynchronous requests using Tornado')
+                log.info('Tornado seems to be installed, so will use')
             elif HAS_REQUESTS:
                 self.backend = 'requests'
-                log.debug('Excuting threaded requests')
+                log.info('Requests seems to be installed, so will use')
 
     def start(self):
-        # Nothing in particular to prepare
+        # Throw errors if backend it not properly configured
         if self.backend not in ('requests', 'tornado'):
-            raise TransportException('Invalid HTTP backend')
+            raise TransportException('Invalid HTTP backend: %s', self.backend)
+        if self.backend == 'requests' and not HAS_REQUESTS:
+            raise TransportException('Trying to use Requests as backend, but it is not installed')
+        if self.backend == 'tornado' and not HAS_TORNADO:
+            raise TransportException('Trying to use Tornado as backend, but it is not installed')
+        # Prepare the tornado backend
+        if self.backend == 'tornado':
+            self.tornado_client = tornado.httpclient.AsyncHTTPClient(max_clients=self.max_clients)
+        elif self.backend == 'requests':
+            # When using requests, we start a threaded pool
+            #   with the size specified using max_clients.
+            # Tornado already has this feature built-in.
+            # We could just start a single thread per request,
+            #   but this leads to infinite number of threads started withing
+            #   the Publiser processes. Python is really really bad to collect
+            #   the garbage from continuous processes, and this can lead
+            #   to memory leaks.
+            # So we'll queue the messages to be publishe
+            self._publish_queue = queue.Queue()  # TODO: use opts hwm
+            self._pool = []
+            for index in range(self.max_clients):
+                thread = threading.Thread(target=self._publish_requests)
+                thread.daemon = True
+                thread.start()
+                self._pool.append(thread)
 
     def publish(self, obj):
         data = napalm_logs.utils.unserialize(obj)
         if self.backend == 'tornado':
-            pass
+            self.tornado_client.fetch(self.address,
+                                      callback=self._handle_tornado_response,
+                                      method=self.method,
+                                      headers=self.headers,
+                                      auth_username=self.username,
+                                      auth_password=self.password,
+                                      body=str(data),
+                                      validate_cert=self.verify_ssl,
+                                      allow_nonstandard_methods=True,
+                                      decompress_response=False)
         elif self.backend == 'requests':
-            thread = threading.Thread(target=self._publish_requests,
-                                      args=(data,))
-            thread.daemon = True
-            thread.start()
+            # Queue the publish object async
+            self._publish_queue.put_nowait(data)
 
-    def _publish_requests(self, data):
-        session = requests.Session()
-        session.auth = self.auth
-        session.headers.update(self.headers)
-        session.verify = self.verify_ssl
-        result = session.request(
-            self.method,
-            self.address,
-            params=self.params,
-            data=data
-        )
-        if not result.ok:
-            log.error('Unable to publish to %s', self.address)
-            log.error('Status code: %d', result.status_code)
-            log.error(result.text)
-        else:
-            log.debug(result.text)
+    def _publish_requests(self):
+        while True:
+            # 1s timeout so that if the parent dies this thread will die within 1s
+            try:
+                try:
+                    data = self._publish_queue.get(timeout=1)
+                    self._publish_queue.task_done()  # Mark the task as done once we get it
+                except queue.Empty:
+                    # If empty, try again
+                    continue
+            except AttributeError:
+                # During shutdown, `queue` may not have an `Empty` atttribute
+                continue
+            session = requests.Session()
+            session.auth = self.auth
+            session.headers.update(self.headers)
+            session.verify = self.verify_ssl
+            try:
+                result = session.request(
+                    self.method,
+                    self.address,
+                    params=self.params,
+                    data=data
+                )
+                if not result.ok:
+                    log.error('Unable to publish to %s', self.address)
+                    log.error('Status code: %d', result.status_code)
+                    log.error(result.text)
+                else:
+                    log.debug(result.text)
+                del result
+            except requests.ConnectionError as conn_err:
+                log.error(conn_err)
+            del session
 
     def _handle_tornado_response(self, response):
         if response.error:
-            log.error('Unable to push to %s', self.address)
+            log.error('Unable to publish to %s', self.address)
             log.error(response.error)
         else:
             log.debug(response.body)
 
     def stop(self):
-        # Nothing in particular to shutdown
-        return
+        if self.backend == 'tornado':
+            self.tornado_client.close()
+        elif self.backend == 'requests':
+            for thread in self._pool:
+                thread.join()
