8c05019bffe7088b4d5a7d70375d5ef134fb2ab4
Mircea Ulinic <mirucha@cloudflare.com>
2018-05-07 21:32:01 +0100

    Adapt OS identifier for NX-OS for a different message format
    Of course the format is different across versions, e.g.,
    2018 Apr 20 13:15:07 nexus-switch %ETHPORT-5-IF_DOWN_LINK_FAILURE: Interface Ethernet1/33 is down (Link failure)

--- napalm_logs/config/nxos/__init__.py
+++ napalm_logs/config/nxos/__init__.py
@@ -19,11 +19,23 @@ _RGX_PARTS = [
     ('message', r'(.*)')
 ]
 _RGX_PARTS = OrderedDict(_RGX_PARTS)
-
 _RGX = r'\<{0[pri]}\>{0[host]}: {0[date]} {0[time]} {0[timeZone]}: %{0[tag]}: {0[message]}'.format(_RGX_PARTS)
 
+_ALT_RGX_PARTS = [
+    ('pri', r'(\d+)'),
+    ('date', r'(\d+ \w+ +\d+)'),
+    ('time', r'(\d\d:\d\d:\d\d)'),
+    ('host', r'([^ ]+)'),
+    ('tag', r'([\w\d-]+)'),
+    ('message', r'(.*)')
+]
+_ALT_RGX_PARTS = OrderedDict(_ALT_RGX_PARTS)
+_ALT_RGX = r'\<{0[pri]}\>{0[date]} {0[time]} {0[host]} %{0[tag]}: {0[message]}'.format(_ALT_RGX_PARTS)
+
 _TIME_FORMAT = ('{date} {time} {timeZone}', '%Y %b %d %H:%M:%S %Z')
+_ALT_TIME_FORMAT = ('{date} {time}', '%Y %b %d %H:%M:%S')
 
 
 def extract(msg):
-    return napalm_logs.utils.extract(_RGX, msg, _RGX_PARTS, _TIME_FORMAT)
+    return napalm_logs.utils.extract(_RGX, msg, _RGX_PARTS, _TIME_FORMAT) or\
+           napalm_logs.utils.extract(_ALT_RGX, msg, _ALT_RGX_PARTS, _ALT_TIME_FORMAT)
