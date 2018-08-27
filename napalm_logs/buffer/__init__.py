bc007c1844c6b3914474727b60dfabc956240ce0
Mircea Ulinic <mirucha@cloudflare.com>
2018-08-27 23:18:12 +0100

    Add buffer interface eggs

---
+++ napalm_logs/buffer/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+'''
+napalm-logs pluggable buffer interface.
+'''
+from __future__ import absolute_import, unicode_literals
+
+# Import python std lib
+import logging
+
+# Import napalm-logs pkgs
+# Exceptions
+from napalm_logs.exceptions import InvalidBufferException
+
+log = logging.getLogger(__file__)
+
+BUFFER_LOOKUP = {
+    'mem': memory.buffer,
+    'redis': redis.buffer
+}
+
+
+def get_interface(name):
+    '''
+    Return the serialize function.
+    '''
+    try:
+        log.debug('Using %s as buffer interface', name)
+        return BUFFER_LOOKUP[name]
+    except KeyError:
+        msg = 'Buffer interface {} is not available'.format(name)
+        log.error(msg, exc_info=True)
+        raise InvalidBufferException(msg)
+
+
+__all__ = (
+    'get_interface',
+)
