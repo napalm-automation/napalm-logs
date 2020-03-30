6d6b97bcfdd5b081d965c12b0170b284de520113
Mircea Ulinic <mulinic@digitalocean.com>
2020-03-30 13:08:14 +0100

    PyYAML yaml.load(input) Deprecation
    See https://msg.pyyaml.org/load for more details.

--- napalm_logs/base.py
+++ napalm_logs/base.py
@@ -308,7 +308,7 @@ class NapalmLogs:
                     try:
                         log.debug('Loading %s as YAML', file_)
                         with open(filepath, 'r') as fstream:
-                            cfg = yaml.load(fstream)
+                            cfg = yaml.load(fstream, Loader=yaml.FullLoader)
                             # Reposition at the top and read the comments.
                             if file_name not in CONFIG.OS_INIT_FILENAMES:
                                 # If the file name is not a profile init.
