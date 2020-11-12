06bdc67ebba4187605a9fd2984ea18e572bcaa7e
Mircea Ulinic <mirceaulinic@users.noreply.github.com>
2020-11-12 16:28:54 +0000

    Decrease log level for TCP timeout

--- napalm_logs/listener/tcp.py
+++ napalm_logs/listener/tcp.py
@@ -95,7 +95,7 @@ class TCPListener(ListenerBase):
         except socket.timeout:
             if not self.__up:
                 return
-            log.error('Connection %s:%d timed out', addr[0], addr[1])
+            log.info('Connection %s:%d timed out', addr[0], addr[1])
         finally:
             log.debug('Closing connection with %s', addr)
             conn.close()
